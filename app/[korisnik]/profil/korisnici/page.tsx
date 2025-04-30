d701624186bfd08c8cf74dae46045242866bc3af
Nikola Davidov <ndavidov120rn@raf.rs>
2025-04-30 13:26:01 +0200

    Korisnici

---
+++ app/[korisnik]/profil/korisnici/page.tsx
@@ -0,0 +1,11 @@
+import KorisniciTable from "@/components/KorisniciTable";
+
+const korisnici=()=>{
+    return(
+        <div>
+            <KorisniciTable title="Korisnici"/>
+        </div>
+    )
+}
+
+export default korisnici;
