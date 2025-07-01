dc02a97cff02a57fa67cb393b818940c3eb579a3
unknown <dusan.strbac01@gmail.com>
2025-07-01 15:03:12 +0200

    .

--- app/[korisnik]/admin/page.tsx
+++ app/[korisnik]/admin/page.tsx
@@ -133,7 +133,8 @@ const admin = () => {
   useEffect(() => {
     async function fetchArtikle() {
       try {
-        // Promeniti fetch da cita artikle iz parametara, takodje tamo da ih i upisuje da bi se ova metoda obrisala
+        // Promeniti fetch da cita artikle iz parametara, takodje tamo da ih i upisuje da bi se ova metoda obrisala.
+        // Promeniti fetch da ne postoji vise ovaj DajArtikle jer je los
         const res = await fetch(`${apiAddress}/api/Artikal/DajArtikle`);
         const data: artikalProp[] = await res.json();
         setArticleList(data);
