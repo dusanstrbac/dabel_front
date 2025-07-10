4fb3b0c8ba278c6a270e1cf43656ae7d6a147386
unknown <dusan.strbac01@gmail.com>
2025-07-10 13:32:34 +0200

    Najprodavaniji artikli

--- app/(auth)/aktivacija/AktivacijaPage.tsx
+++ app/(auth)/aktivacija/AktivacijaPage.tsx
@@ -35,7 +35,7 @@ export default function AktivacijaNaloga() {
       setLoading(true);
       const apiAddress = process.env.NEXT_PUBLIC_API_ADDRESS;
 
-      const response = await axios.post(`${apiAddress}/api/Partner/PostaviLozinku`, {
+      const response = await axios.post(`http://10.0.0.38:7235/api/Partner/PostaviLozinku`, {
         Token: token,
         Lozinka: lozinka,
       });
