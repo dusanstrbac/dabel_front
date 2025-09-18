5601f5d324db9322c7f71c5dfcabd7c3865b52f1
unknown <dusan.strbac01@gmail.com>
2025-09-18 13:12:51 +0200

    Jezik update

--- app/[locale]/(auth)/login/LoginForm.tsx
+++ app/[locale]/(auth)/login/LoginForm.tsx
@@ -140,7 +140,7 @@ export default function LoginForm() {
             {loading ? (
               <>
                 <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                {t('Prijavljivanje')}
+                {t('Logovanje.Prijavljivanje')}
               </>
             ) : (
               t('Logovanje.Prijavi se')
